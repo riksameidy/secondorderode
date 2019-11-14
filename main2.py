6a41cf29e662b8576fc3fc840012ffb23bac1cba
Riksa Meidy <riksameidy@gmail.com>
2019-11-14 08:16:40 +0700

    add nonlinear pendulum oscilaltion

---
+++ main2.py
@@ -0,0 +1,55 @@
+from solver import *
+from math import *
+import matplotlib.pyplot as plt
+
+g = 9.81                                    # gravitational Accelaration
+l = 1                                       # pendulum length
+k = 0                                       # velocity coeff
+u0 = 0.5 * pi
+du0 = 0
+t0 = 0
+t_akhir = 4
+h = 0.01
+w0 = g/l
+
+def Func(t,u,du):
+    return -w0 * sin(u) - k*du
+
+res_eulercromer = []
+res_verlet = []
+t = []
+step = int((t_akhir - t0) / h)
+
+for i in range(step):
+    tm = (i + 1) * h
+    (u_next, du_next) = euler_cromer(tm, h, u0, du0, Func)
+    res_eulercromer.append(u_next)
+    t.append(tm)
+    u0 = u_next
+    du0 = du_next
+
+t = []
+u0 = 0.5 * pi
+du0 = 0
+d2u0 = Func(t0,u0,du0)
+
+for i in range(step):
+    tm = (i + 1) * h
+    (u_next, du_next,d2u_next) = verlet(tm, h, u0, du0, d2u0, Func)
+    res_verlet.append(u_next)
+    t.append(tm)
+    u0 = u_next
+    du0 = du_next
+    d2u0 = d2u_next
+
+plt.title('Non Linear Pendulum h =0.01')
+plt.plot(t,res_eulercromer,color='r', label = 'Euler Cromer')
+plt.plot(t,res_verlet,color='b', label = 'Verlet')
+plt.xlabel('t')
+plt.ylabel('u(t)')
+plt.legend()
+
+plt.show()
+
+
+
--- solver.py
+++ solver.py
@@ -19,3 +19,17 @@ def verlet(t,h,y,dy,d2y,Func):
     d2y_next = Func(t,y_next,dy1_2)
     dy_next = dy1_2 + (h1_2 * d2y_next)
     return (y_next,dy_next,d2y_next)
+
+def verlet2DNewton(m ,h,rx,ry, vx, vy , ax, ay, Forces):
+    h1_2 = 0.5 * h
+    vx1_2 = vx + (h1_2 * ax) ; vy1_2 = vy + (h1_2 * ay)
+    rx_next = rx + (h * vx1_2) ; ry_next = ry + (h * vy1_2)
+
+    (fx,fy,Epot) = Forces(m,rx_next,ry_next,vx1_2,vy1_2)
+    ax_next = fx/m ; ay_next = fy/m
+
+    vx_next = vx1_2 + (h1_2 * ax_next) ; vy_next = vy1_2 + (h1_2 * ay_next)
+
+    Ekin = 1/2 * m * (vx**2 + vy**2)
+
+    return (rx_next,ry_next,vx_next,vy_next,ax_next,ay_next,Ekin,Epot)
